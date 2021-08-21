0dd5e53d8808e6826c44c9ab2984059299adcf0f
Abdun Nur Tomal <tomal.nur@gmail.com>
2021-08-21 12:06:43 +0600

    forget

--- lib/auth/forget_password.dart
+++ lib/auth/forget_password.dart
@@ -1,11 +1,9 @@
-import 'package:chat_app/utils/constants.dart';
-import 'package:chat_app/utils/global_methods.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../utils/global_methods.dart';
 import '../utils/custom_color.dart';
 
 class ForgetPasswordScreen extends StatefulWidget {
@@ -18,8 +16,8 @@ class ForgetPasswordScreen extends StatefulWidget {
 class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final _forgetPasswordFormKey = GlobalKey<FormState>();
-  final TextEditingController _forgetPassTextController =
-  TextEditingController(text: '');
+  late TextEditingController _forgetPassTextController =
+      TextEditingController(text: '');
 
   @override
   void dispose() {
@@ -29,106 +27,99 @@ class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Dialog(
-        insetPadding: const EdgeInsets.all(0.0),
-    backgroundColor: CustomColors.primaryColor,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8.0),
-    ),
-    child: Dialog(
+    return Dialog(
         backgroundColor: CustomColors.primaryColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              width: Constants.kDefaultSizeWidth,
-              color: CustomColors.primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Forget password',
-                      textAlign: TextAlign.center,
-                      style:
+            child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            width: 400,
+            color: CustomColors.primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Forget password',
+                  textAlign: TextAlign.center,
+                  style:
                       GoogleFonts.openSans(color: Colors.white, fontSize: 28),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Enter yout Email address',
-                      textAlign: TextAlign.center,
-                      style:
-                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
-                    ),
-                    const SizedBox(height: 20),
-                    Form(
-                      key: _forgetPasswordFormKey,
-                      child: Container(
-                        padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                            color: CustomColors.secondaryColor,
-                            border: Border.all(color: Colors.blue)),
-                        child: TextFormField(
-                          controller: GlobalMethod.forgetPassTextController,
-                          validator: GlobalMethod.validateEmail,
-                          style: GoogleFonts.openSans(color: Colors.white),
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-                              labelText: 'Email Address',
-                              labelStyle: GoogleFonts.openSans(color: Colors.white),
-                              icon: const Icon(Icons.email, color: Colors.white),
-                              border: InputBorder.none),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    MaterialButton(
-                      elevation: 0,
-                      minWidth: double.maxFinite,
-                      height: 50,
-                      onPressed: _forgetPasFCT,
-                      color: CustomColors.logoGreen,
-                      child: Padding(
-                        padding: const EdgeInsets.all(14.0),
-                        child: Text('Reset now',
-                            style: GoogleFonts.openSans(
-                                color: Colors.white, fontSize: 16)),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'Remebered password',
-                            style: GoogleFonts.openSans(
-                                color: Colors.white, fontSize: 14),
-                          ),
-                          const TextSpan(text: '    '),
-                          TextSpan(
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () => Navigator.canPop(context)
-                                  ? Navigator.pop(context)
-                                  : null,
-                            text: 'Login',
-                            style: GoogleFonts.openSans(
-                                decoration: TextDecoration.underline,
-                                color: Colors.blue.shade300,
-                                //fontWeight: FontWeight.bold,
-                                fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
                 ),
-              ),
-            )))));
+                SizedBox(height: 20),
+                Text(
+                  'Enter yout Email address',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
+                ),
+                SizedBox(height: 20),
+                Form(
+                  key: _forgetPasswordFormKey,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                        color: CustomColors.secondaryColor,
+                        border: Border.all(color: Colors.blue)),
+                    child: TextFormField(
+                      controller: GlobalMethod.forgetPassTextController,
+                      validator: GlobalMethod.validateEmail,
+                      style: GoogleFonts.openSans(color: Colors.white),
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                          labelText: 'Email Address',
+                          labelStyle: GoogleFonts.openSans(color: Colors.white),
+                          icon: Icon(Icons.email, color: Colors.white),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: _forgetPasFCT,
+                  color: CustomColors.logoGreen,
+                  child: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Text('Reset now',
+                        style: GoogleFonts.openSans(
+                            color: Colors.white, fontSize: 16)),
+                  ),
+                ),
+                SizedBox(height: 20),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Remebered password',
+                        style: GoogleFonts.openSans(
+                            color: Colors.white, fontSize: 14),
+                      ),
+                      TextSpan(text: '    '),
+                      TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => Navigator.canPop(context)
+                              ? Navigator.pop(context)
+                              : null,
+                        text: 'Login',
+                        style: GoogleFonts.openSans(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue.shade300,
+                            //fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )));
   }
 
   void _forgetPasFCT() async {
